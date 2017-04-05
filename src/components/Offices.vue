5c036113647ca9bf7d222785d89b396752084062
kyle <the.kyle.maune@gmail.com>
2017-04-05 06:53:48 -0700

    created offices component - offices.vue

---
+++ src/components/Offices.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="offices">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'offices',
+  data () {
+    return {
+      msg: 'Cooper Aerial | Offices'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+</style>
