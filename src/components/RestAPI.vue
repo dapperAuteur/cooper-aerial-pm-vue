b60505f5882d65c8b91ee8de3662c258ce8fdcaf
dapperAuteur <a@awews.com>
2017-04-10 13:58:04 -0700

    display list of companies on page in list

--- src/components/RestAPI.vue
+++ src/components/RestAPI.vue
@@ -2,13 +2,21 @@
   <div class="restA">
     <h1>RestAPI</h1>
     <router-view></router-view>
+    <ul>
+      <li v-for="company in companies">{{ company.id }} {{ company.name }} {{ company.website }}</li>
+    </ul>
   </div>
 </template>
 <script>
   export default {
     name: 'restA',
+    data(){
+      return {
+        companies: []
+      }
+    },
     mounted(){
-      axios.get('//cuddly-robot-api.herokuapp.com/api/companies').then(response => console.log(response.data));
+      axios.get('//cuddly-robot-api.herokuapp.com/api/companies').then(response => this.companies = response.data);
     }
   }
 </script>
